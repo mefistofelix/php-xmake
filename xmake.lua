a335cb4a70e7a5089b15038b8cb3e092c611c70c
mefistofelix <1835312+mefistofelix@users.noreply.github.com>
2026-08-12 15:27:18 +0200

    build: group liblzma encoder header users

--- xmake.lua
+++ xmake.lua
@@ -189,7 +189,8 @@ target("liblzma")
         "in/deps/xz/src/liblzma/common/stream_encoder.c", {unity_group = "conflict_3"})
     add_files("in/deps/xz/src/liblzma/common/alone_encoder.c",
         "in/deps/xz/src/liblzma/common/index_encoder.c",
-        "in/deps/xz/src/liblzma/lzma/lzma_encoder.c", {unity_group = "conflict_4"})
+        "in/deps/xz/src/liblzma/lz/lz_encoder_mf.c",
+        "in/deps/xz/src/liblzma/lzma/lzma_encoder*.c", {unity_group = "conflict_4"})
 
 target("php")
     set_enabled(false)
