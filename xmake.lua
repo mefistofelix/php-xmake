a5821aaf7567491a685e3869407b0dbe6cb7d824
mefistofelix <1835312+mefistofelix@users.noreply.github.com>
2026-08-13 21:23:40 +0200

    Broaden OpenSSL NIST source exclusion

--- xmake.lua
+++ xmake.lua
@@ -963,7 +963,7 @@ target("openssl_test")
     end)
 
     add_files("in/deps/openssl/**/*.c")
-    remove_files("in/deps/openssl/crypto/ec/ecp_nistp*.c")
+    remove_files("in/deps/openssl/crypto/ec/ecp_nist*.c")
 
     add_asflags("-Ox", "-f", "win64", "-DNEAR", "-g", {force = true})
     add_files("in/deps/openssl/**/*x86_64*.asm")
